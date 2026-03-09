e14e00744973fc35b55462f9879b925411dc0361
swirl <180882683+lolswirl@users.noreply.github.com>
2026-03-09 15:44:10 -0400

    [feature] remove basic minimap profiling

--- Strings.lua
+++ Strings.lua
@@ -1,26 +1,6 @@
 local _, SwirlUI = ...
 
 SwirlUI.ImportProfiles = {
-    {
-        name = "BasicMinimap",
-        short = "BasicMinimap",
-        displayName = "BasicMinimap (1080p)",
-        version = "1.0",
-        targetProfile = SwirlUI.ProfileTenEightyP,
-        string = "Tsv0UjomqWVM(2rLDsacpcCWvKqHE1PVCpyj3ehWQj2iBhDT81F76ekqUkD6KQuvXMSZm7oZg4uEoNDqvkNxwA0o(coRr5Ck88lCMRqul5zuo7KrlZLV5xA0vQ9iktRVwPLC2UNZ3UjBfNvz0EM6eciIZe1Q9AoB7Q1qXngTP4G10irAlm1gBMX2iQbMa6X)I4zK7tH)hdh4zj4x67RLj0f(wH99(Yj3N8bIoa3pl5cM35zJOJ5S3WN1XWdgNxbZYTYr7bthO3ARsQlRVixhGpR8AHZfW8XGbwKx68YYbInD4OH(fNX(TYw3dDUvQf)ZE0JXHPzHxh0S2u86)DSSCvw(QNggmGXrNg8n6NoA30ADng9ZNh0ASIkVsVFNTugO4fdEQVvqgH75QMUB4uGRFHXPtq4XpALfkCDKZUR8B3vc9FR38ly6XADbQIIiq1WIQUuyrtHLzqdWkkvTo0X7A(fb5phPDU7Wi5LA1PtlA9EJERPq4rLbtvGVDS00Cuy9nsykZOXrDPWgnWrukNvQQQufT1E0ehtU6fPm6m8QqvdIEvVcah3rYUwGYys0qtdsdc2wNP6QPRrC8Mzvv(DOb2uy6xkUo7(R2)S96(62BiH1gYxWAtOXqShnovFcGBi57E8Pn)4HCKLRUeZxpkjnHmlojHstNerGv4QrJdP66rtMozcDCA6ukHetbdhEuq2qEaQ4TIIxbh)k30DqCe(G9ZNN)0Ql)2huTNZ)Zd",
-        database = BasicMinimapSV,
-        color = "9bf162",
-    },
-    {
-        name = "BasicMinimap",
-        short = "BasicMinimap1440p",
-        displayName = "BasicMinimap (1440p)",
-        version = "1.0",
-        targetProfile = SwirlUI.ProfileFourteenFortyP,
-        string = "1fzYonmmqy4NOIIJtItU2skujKlQoNhj3SPrn1gzBeL(0Zydi2e3M1)5Z)gyqhOoJEpAnE4iO896LrqYUHZb1vRzSB8syJ1mHZXr1l4Sbup0ULsMSMaOuVGUfq9kixXkb1fqYbQCVtpfqZ8E3WOlQ8rBmsHxj5Zb1IT)eSMMlfeV87DYZOAedMbTBDGULKGauo9a(ShKzXqC4wCAAxV1exmPWFqCtRSR9qIhwIoM4Nehz0smLE2pz9yG8asG44D7F8WU7UVlr6xjCy7krUOPklRI1WlyCrnmTQKbYcQvzwTGZffnvnnf18pAr35SgxsKMU3)I6V9ZS4UbNU)e5JFZl(8BHKmaWB)",
-        database = BasicMinimapSV,
-        color = "9bf162",
-    },
 }
 
 SwirlUI.ApplyAddons = {
