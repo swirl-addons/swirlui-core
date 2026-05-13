9d07c7170da0e5f268ae414ae0b35a755161e87d
swirl <180882683+lolswirl@users.noreply.github.com>
2026-05-13 17:47:32 -0400

    color mixin and font array set

--- UI/Util.lua
+++ UI/Util.lua
@@ -5,10 +5,11 @@ local C = SUI.Components
 function C.T() return SUI.Theme end
 
 function C.ApplyFont(fs, size)
-    local sz = size == "small" and SUI.FontSize
-           or  size == "large" and (SUI.FontSize + 2)
-           or  SUI.FontSize
-    fs:SetFont(SUI.Font, sz, "OUTLINE")
+    local font = C.T().font
+    local sz = size == "small" and font.size.small
+           or  size == "large" and (font.size.normal + 2)
+           or  font.size.normal
+    fs:SetFont(font.ttf, sz, "OUTLINE")
     fs:SetShadowOffset(0, 0)
     fs:SetShadowColor(0, 0, 0, 0)
 end
@@ -20,8 +21,8 @@ function C.SetBackdrop(frame, bg, border)
         edgeFile = "Interface\\Buttons\\WHITE8X8",
         edgeSize = theme.borderSize,
     })
-    if bg     then frame:SetBackdropColor(bg[1], bg[2], bg[3], bg[4]) end
-    if border then frame:SetBackdropBorderColor(border[1], border[2], border[3], border[4]) end
+    if bg     then frame:SetBackdropColor(bg.r, bg.g, bg.b, bg.a) end
+    if border then frame:SetBackdropBorderColor(border.r, border.g, border.b, border.a) end
 end
 
 function C.AnimateBorderColor(frame, animGroup, fromColor, toColor)
